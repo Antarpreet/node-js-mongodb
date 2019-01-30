88a451c31bef613c32e49f8dbc467c9ee4750b0d
Antarpreet Singh <antarpreet13@gmail.com>
2019-01-29 20:28:45 -0800

    Add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,14 +6,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    // db.collection('Todos').find({
-    //     _id: new ObjectID('5c51126343fa209b44f5e639')
-    // }).toArray().then(docs => {
-    //     console.log('todos');
-    //     console.log(JSON.stringify(docs, undefined, 2));
-    // }).catch(err => {
-    //     console.log('Unable to fetch todos', err);
-    // });
+    db.collection('Todos').find({
+        _id: new ObjectID('5c51126343fa209b44f5e639')
+    }).toArray().then(docs => {
+        console.log('todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }).catch(err => {
+        console.log('Unable to fetch todos', err);
+    });
 
     db.collection('Users').find().count().then(count => {
         console.log('todos count: ' + count);
